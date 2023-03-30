b11a163e4f04b198c05b7134e693f5dd1efb81b1
Ganesh Kandhan V M <87404827+ganeshkandhan17@users.noreply.github.com>
2023-03-30 22:04:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let count=0;
+let todolist=[];
 function addlist(){
 let form = document.getElementById('todo-form');
 form.addEventListener('submit', function(offdefault) {
@@ -9,28 +11,31 @@ offdefault.preventDefault();
     if(content==""){
         alert("Value unga appan vanthu pooduvaana?????");
     }
+    else if(todolist.includes(content)){
+        alert("iethu already iruku mame!!!");
+    }
     else{
     nlist.innerHTML=content;
     list.appendChild(nlist);
+    todolist.push(content);
+    document.getElementById("in").value="";
     }
 }
-
+//==========================================================
 function dellsttag(){
     try{ let del=document.querySelector("ol li:last-child");
-    del.remove();}
+    del.remove();
+    todolist.splice((todolist.length-1),1);
+}
     catch(error){
         alert("anga onnume ielada koomutta!!!");
+
     }
 }
-
+//===========================================================
 const mediaQuery = window.matchMedia("(max-width: 600px)");
-
 function handleScreenSizeChange(mediaQuery) {
   if (mediaQuery.matches) {
     document.body.style.backgroundSize = "stretch";
 }
 }
-
-handleScreenSizeChange(mediaQuery);
-
-mediaQuery.addListener(handleScreenSizeChange);
